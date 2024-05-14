8c72b2f177622a18a2fa33d61be93d789eac43e4
SeungGyu <phillipskl123@gmail.com>
2024-05-14 19:45:04 +0900

    [refactor] 패지키 구조 리팩토링

--- src/main/java/com/cow/cow_mvc_practice/member/controller/MemberController.java
+++ src/main/java/com/cow/cow_mvc_practice/member/controller/MemberController.java
@@ -2,13 +2,7 @@ package com.cow.cow_mvc_practice.member.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cow.cow_mvc_practice.member.controller.dto.request.MemberRequest;
 import com.cow.cow_mvc_practice.member.controller.dto.response.MemberResponse;
@@ -23,25 +17,18 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	/* 기본 */
-	// @PostMapping("/new")
-	// public String create(@RequestBody final MemberRequest memberRequest) {
-	// 	memberService.join(memberRequest);
-	// 	return "회원저장 성공!";
-	// }
-	//
-	// @GetMapping("/{memberId}")
-	// public String findMember(@PathVariable final Long memberId) {
-	// 	Member member = memberService.findOne(memberId);
-	// 	return "member 아이디: " + member.getId() + ", member 이름: " + member.getName();
-	// }
-	//
-
 	/* MemberResponse dto 적용 */
 	@PostMapping("/new")
 	public MemberResponse create(@RequestBody final MemberRequest memberRequest) {
+		System.out.println("111");
 		return memberService.join(memberRequest.getName());
 	}
+
+//	@PatchMapping("/{memberId}")
+//	public MemberResponse update(@PathVariable("memberId") String memberId, @RequestBody final MemberRequest memberRequest) {
+//
+//		return memberService.
+//	}
 
 	@GetMapping("/{memberId}")
 	public MemberResponse findMember(@PathVariable final Long memberId) {
