222749ef6888cf8188694a981718ba0a6e076d9f
fuirian <kmjkhj2017@gmail.com>
2024-05-21 19:44:20 +0900

    8주차 과제 수정

---
+++ src/main/java/com/cow/cow_mvc_practice/comment/controller/dto/request/UpdateCommentRequest.java
@@ -0,0 +1,2 @@
+package com.cow.cow_mvc_practice.comment.controller.dto.request;public class UpdateCommentRequest {
+}
---
+++ src/main/java/com/cow/cow_mvc_practice/comment/repository/CommentRepository.java
@@ -0,0 +1,2 @@
+package com.cow.cow_mvc_practice.comment.repository;public interface CommentRepository {
+}
